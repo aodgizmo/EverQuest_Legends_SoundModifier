8bb29e379ab58c33d84d4561df1b614b93d6a423
aodgizmo <aodgizmo@gmail.com>
2026-09-10 02:21:51 -0500

    Update RemoveAnnoyingSounds.bat

--- RemoveAnnoyingSounds.bat
+++ RemoveAnnoyingSounds.bat
@@ -1,4 +1,4 @@
-u/echo off
+@echo off
 powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "$f='%~dp0soundassets.txt'; $r=@('143^thunder1.wav^','144^thunder2.wav^','159^rainloop.wav^','4624^dog_atk.wav^','566^wolf_dire_atk.wav^','355^Wolf_Att.wav^','351^WereWAtt.wav^'); $c=Get-Content -LiteralPath $f; $c | Where-Object { $r -notcontains $_ } | Set-Content -LiteralPath $f -Encoding ASCII"
 echo Done.
 pause
